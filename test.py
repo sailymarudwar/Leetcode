e109a61ca9c7f28229b3bd3ba75a11d1888aca27
Saily Marudwar <sayali.m28@gmail.com>
2020-10-26 16:34:47 -0500

    New codes

--- test.py
+++ test.py
@@ -1,36 +1,8 @@
-'''class Testing:
-    def __init__(self):
-        self.__score = 95
+s = 'asdabbadfr'
 
-    def test(self):
-        print("Score {}".format(self.__score))
+i = 0
+j = (len(s)-1)
 
-    def ms(self,score):
-        self.__score = score
-        print("Max score: {}".format(self.__score))
-
-results = Testing()
-print(results.test())
-
-print(results.ms(100))
-print(results.test())'''
-
-import sys, traceback, pdb
-
-def lumberjack():
-    bright_side_of_death()
-
-def bright_side_of_death():
-    return tuple()[0]
-def debug(e, i, tb):
-    traceback.print_exception(e, i, tb)
-    pdb.pm()
-
-try:
-    lumberjack()
-except IndexError:
-    e, i, tb = sys.exc_info()
-    traceback.excepthook = debug(e, i, tb)
-
-
-
+print(s[i:j])
+print(s[i:j][::-1])
+print(s[::-1])
