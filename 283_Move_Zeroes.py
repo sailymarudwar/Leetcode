6958664f28c7f94b150afc38d1675e41c20f7ddf
sailymarudwar <sayali.m28@gmail.com>
2020-11-17 19:03:57 -0600

    Completed Code

--- 283_Move_Zeroes.py
+++ 283_Move_Zeroes.py
@@ -8,3 +8,17 @@ Note:
 
 You must do this in-place without making a copy of the array.
 Minimize the total number of operations.'''
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = count = 0
+        while count < len(nums):
+            if nums[i] == 0:
+                nums.append(nums.pop(i))
+            else:
+                i += 1
+            count += 1
